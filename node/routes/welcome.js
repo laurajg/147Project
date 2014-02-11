e9a79bc7f4f19fa6eb0af5f881de2cb9d2dbfe1b
amberr <arrockw@gmail.com>
2014-02-11 18:26:54 +0000

    added changePassword function

--- node/routes/welcome.js
+++ node/routes/welcome.js
@@ -1,5 +1,6 @@
 exports.view = function(req, res){
 	var dbUtils = require('dbUtils');
 	dbUtils.createUser(req.query.username, req.query.email, req.query.password);
+	req.session.user = req.query.username;
 	res.render('welcome');
 };
