3d32b03e229d3ae77bbe274e371cf7ad1cf36b1a
asifrkhan <asifkhan89@gmail.com>
2014-02-14 00:07:14 -0800

    fixed some text image stuff

--- node/public/js/index.js
+++ node/public/js/index.js
@@ -3,7 +3,15 @@ var img_list = []
 function setRandomPhoto(photos) {
 	img_list = photos;
 	var photo = photos[Math.floor(Math.random()*photos.length)];
-	$("#randomImg").attr('src', photo['url']);
+	var html = "";
+	if (photo['url'].indexOf('text://') == 0) {
+		var text = photo['url'].substring(7);
+		html = '<h3 style="text-align:center">'+text+'</h3>';
+
+	} else {
+		html = '<img id="randomImg" class="img-rounded  img-responsive" src="'+photo['url']+'"/>';			
+	}
+	$("#Randomimage").html(html);		
 }
 
 $(".submittButton .changePic").click(function(e) {
