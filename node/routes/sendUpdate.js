b1f0e5d073dc799a62bf0a36fa46da6f29066b23
asifrkhan <asifkhan89@gmail.com>
2014-02-14 00:18:16 -0800

    fixed labels

--- node/routes/sendUpdate.js
+++ node/routes/sendUpdate.js
@@ -1,11 +1,13 @@
 exports.sendUpdate = function(req, res){
 	var dbUtils = require('dbUtils');
-	var mailUtils = require('mailUtils');
-	mailUtils.initialize();	
-	var subject_text = 'GoalGlance: New update from ' + req.session.user + '!';
-	var message = '<p>'+ req.session.user + ' has a new update about their goal!</p><p><b>'+req.query.message+'</b></p>'
-	message += '<p>Click <a href="http://goalglance.herokuapp.com/">here</a> to send ' + req.session.user + ' some encouragement![TODO]</p>';
-	mailUtils.sendMail('asifkhan89@gmail.com',subject_text,message);
-	console.log(req.query);
+	dbUtils.getEmail(req.session.user, function(emailaddr) {
+		var mailUtils = require('mailUtils');
+		mailUtils.initialize();	
+		var subject_text = 'GoalGlance: New update from ' + req.session.user + '!';
+		var message = '<p>'+ req.session.user + ' has a new update about their goal!</p><p><b>'+req.query.message+'</b></p>'
+		message += '<p>Click <a href="http://goalglance.herokuapp.com/">here</a> to send ' + req.session.user + ' some encouragement![TODO]</p>';
+		mailUtils.sendMail(emailaddr,subject_text,message);
+	});	
+	//console.log(req.query);
 	res.redirect('/gallery');
 };
