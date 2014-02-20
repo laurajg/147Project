fa7baae2e6d00123145892f03b5cb308f96565ca
amberr <arrockw@gmail.com>
2014-02-20 07:14:33 +0000

    fixed back button in intro flow

--- node/routes/welcome.js
+++ node/routes/welcome.js
@@ -1,10 +1,12 @@
 exports.view = function(req, res){
-	if(!req.query.username || !req.query.email || !req.query.password) {
+	if(!req.query.username && !req.query.email && !req.query.password && !req.session.user) {
 		res.redirect('landing');
 	} else {
 		var dbUtils = require('dbUtils');
 		dbUtils.createUser(req.query.username, req.query.email, req.query.password);
-		req.session.user = req.query.username;
+		if (!req.session.user) {
+			req.session.user = req.query.username;
+		}
 		res.render('welcome', {'user': req.session.user});
 	}
 };
