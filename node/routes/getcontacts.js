fb0cd0843ae4216cbbe153e0c49dc9e57644b87e
asifrkhan <asifkhan89@gmail.com>
2014-03-03 02:11:36 -0800

    added google analytics instrumentation

--- node/routes/getcontacts.js
+++ node/routes/getcontacts.js
@@ -3,7 +3,8 @@ exports.view = function(req, res){
 //        res.redirect('landing');
 //    } else {
         var alt = req.query.alt === undefined ? '0' : req.query.alt;                      
-        res.render('getcontacts',{'alt0': alt == 0, 'alt1': alt==1, 'alt2': alt==2});        
+        res.render('getcontacts',{'alt0': alt == 0, 'alt1': alt==1, 'alt2': alt==2, 'statTotalTime': 'statTotalTime'+alt,
+                                    'statPageLoad':'statPageLoad'+alt, 'statAddClicked':'statAddClicked'+alt, 'statTypeToAddTime': 'statTypeToAddTime'+alt});        
 //    }
 };
 exports.addContact = function(req, res) {
