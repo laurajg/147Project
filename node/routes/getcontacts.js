d133666ff5c8e800fd43cdb53fc79b5795dd782d
Laura Griffiths <lauragriffiths@cs.stanford.edu>
2014-03-05 04:58:54 +0000

    fixed spacing on add contacts page, fixed desktop styling, added goal to route and made inaccessible if not logged in

--- node/routes/getcontacts.js
+++ node/routes/getcontacts.js
@@ -1,10 +1,21 @@
 exports.view = function(req, res){
+    if(!req.session.user) {
+        res.redirect('landing');
+    } else {
+        var dbUtils = require('dbUtils');
+        var alt = req.query.alt === undefined ? '0' : req.query.alt;                      
+
+        dbUtils.getGoal(req.session.user, function(goal) {
+            res.render('getcontacts', {'user': req.session.user, 'goal': goal, 'alt0': alt == 0, 'alt1': alt==1, 'alt2': alt==2, 'statTotalTime': 'statTotalTime'+alt,
+                                    'statPageLoad':'statPageLoad'+alt, 'statAddClicked':'statAddClicked'+alt, 'statTypeToAddTime': 'statTypeToAddTime'+alt});
+        });
+    }
 //    if(!req.session.user) {
 //        res.redirect('landing');
 //    } else {
-        var alt = req.query.alt === undefined ? '0' : req.query.alt;                      
-        res.render('getcontacts',{'alt0': alt == 0, 'alt1': alt==1, 'alt2': alt==2, 'statTotalTime': 'statTotalTime'+alt,
-                                    'statPageLoad':'statPageLoad'+alt, 'statAddClicked':'statAddClicked'+alt, 'statTypeToAddTime': 'statTypeToAddTime'+alt});        
+       // var alt = req.query.alt === undefined ? '0' : req.query.alt;                      
+     //   res.render('getcontacts',{'alt0': alt == 0, 'alt1': alt==1, 'alt2': alt==2, 'statTotalTime': 'statTotalTime'+alt,
+       //                             'statPageLoad':'statPageLoad'+alt, 'statAddClicked':'statAddClicked'+alt, 'statTypeToAddTime': 'statTypeToAddTime'+alt});        
 //    }
 };
 exports.addContact = function(req, res) {
